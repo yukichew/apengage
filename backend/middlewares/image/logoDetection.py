31bff7eefbe3e0a0e4e859abd79d98f2493b4e24
Yuki Chew <yukichew0803@gmail.com>
2024-06-10 01:08:58 +0800

    modify logo detection py file

--- backend/middlewares/image/logoDetection.py
+++ backend/middlewares/image/logoDetection.py
@@ -3,31 +3,21 @@ import numpy as np
 import sys
 
 def detect_logo(logo_filename, image_filename):
-    # Load the logo image
     logo_img = cv2.imread(logo_filename, 0)
-
-    # Initialize SIFT detector
     sift = cv2.SIFT_create()
-
-    # Detect keypoints and descriptors for the logo
-    kp_logo, des_logo = sift.detectAndCompute(logo_img, None)
-
-    # Load the image where you want to detect the logo
+    
+    des_logo = sift.detectAndCompute(logo_img, None)
     image = cv2.imread(image_filename)
-
-    # Detect keypoints and descriptors for the image
-    kp_image, des_image = sift.detectAndCompute(image, None)
-
-    # Initialize a FLANN matcher
+    
+    des_image = sift.detectAndCompute(image, None)
     FLANN_INDEX_KDTREE = 1
+    
     index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
     search_params = dict(checks=50)
+    
     flann = cv2.FlannBasedMatcher(index_params, search_params)
-
-    # Match descriptors
     matches = flann.knnMatch(des_logo, des_image, k=2)
-
-    # Ratio test to find good matches
+    
     good_matches = []
     for m, n in matches:
         if m.distance < 0.7 * n.distance:
