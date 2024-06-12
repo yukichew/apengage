9079c1a0e87637a1cfdd0f0341c5f1206223055b
yukichew <yukichew0803@gmail.com>
2024-06-12 16:25:59 +0800

    complete category controllers

--- backend/controllers/event/categoryController.js
+++ backend/controllers/event/categoryController.js
@@ -1,3 +1,5 @@
+const { isValidObjectId } = require('mongoose');
+const { sendError } = require('../../helpers/error');
 const Category = require('../../models/event/category');
 
 exports.createCategory = async (req, res) => {
@@ -16,5 +18,98 @@ exports.createCategory = async (req, res) => {
       name: newCategory.name,
       desc: newCategory.desc,
     },
+  });
+};
+
+exports.updateCategory = async (req, res) => {
+  const { name, desc } = req.body;
+  const { id } = req.params;
+
+  if (!isValidObjectId(id)) return sendError(res, 401, 'Invalid category id');
+
+  const category = await Category.findById(id);
+  if (!category) return sendError(res, 404, 'Category not found');
+
+  category.name = name;
+  category.desc = desc;
+
+  await category.save();
+
+  res.json({
+    category: {
+      id: newCategory._id,
+      name: newCategory.name,
+      desc: newCategory.desc,
+    },
+  });
+};
+
+exports.deleteCategory = async (req, res) => {
+  const { id } = req.params;
+
+  if (!isValidObjectId(id)) return sendError(res, 401, 'Invalid category id');
+
+  const category = await Category.findById(id);
+  if (!category) return sendError(res, 404, 'Category not found');
+
+  await Category.findByIdAndDelete(id);
+
+  res.json({ message: 'Category deleted' });
+};
+
+exports.getCategories = async (req, res) => {
+  const { pageNo, limit = 9 } = req.query;
+
+  const categories = await Category.find({})
+    .sort({ createdAt: -1 })
+    .skip(parseInt(pageNo) * limit)
+    .limit(limit);
+
+  const count = await Category.countDocuments();
+
+  res.json({
+    categories: categories.map((category) => {
+      return {
+        id: category._id,
+        name: category.name,
+        desc: category.desc,
+      };
+    }),
+    count,
+  });
+};
+
+exports.getCategory = async (req, res) => {
+  const { id } = req.params;
+
+  if (!isValidObjectId(id)) return sendError(res, 401, 'Invalid category id');
+
+  const category = await Category.findById(id);
+  if (!category) return sendError(res, 404, 'Category not found');
+
+  res.json({
+    category: {
+      id: category._id,
+      name: category.name,
+      desc: category.desc,
+    },
+  });
+};
+
+exports.searchCategory = async (req, res) => {
+  const { query } = req.query;
+
+  const result = await Category.find({
+    name: { $regex: query.name, $options: 'i' },
+  });
+
+  res.json({
+    categories: result.map((category) => {
+      return {
+        id: category._id,
+        name: category.name,
+        desc: category.desc,
+      };
+    }),
   });
 };
--- backend/routes/event/category.js
+++ backend/routes/event/category.js
@@ -1,5 +1,10 @@
 const {
   createCategory,
+  getCategories,
+  updateCategory,
+  deleteCategory,
+  getCategory,
+  searchCategory,
 } = require('../../controllers/event/categoryController');
 const { authenticate } = require('../../middlewares/auth');
 const { categoryValidator } = require('../../middlewares/validator');
@@ -7,5 +12,10 @@ const { categoryValidator } = require('../../middlewares/validator');
 const router = require('express').Router();
 
 router.post('/create', authenticate, categoryValidator, createCategory);
+router.put('/:id', authenticate, categoryValidator, updateCategory);
+router.delete('/:id', authenticate, deleteCategory);
+router.get('/:id', authenticate, getCategory);
+router.get('/categories', authenticate, getCategories);
+router.get('/search', authenticate, searchCategory);
 
 module.exports = router;
